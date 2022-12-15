6a2f17473e5fa81047586954864954fd422900a7
Enzofromspace <8026658+Enzofromspace@users.noreply.github.com>
2022-12-15 19:19:49 +0300

    Finished excersice and fixed CSS for blur variable

--- index.js
+++ index.js
@@ -2,3 +2,13 @@
 import './style.css';
 
 // Write Javascript code!
+const inputs = document.querySelectorAll('.controls input');
+
+function handleUpdate(){
+   const suffix = this.dataset.sizing || '';   
+ //  console.log(this.name);
+   document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
